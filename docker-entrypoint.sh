b4464f3675bdb07ad9a54dd9bd55099622fe19e8
Lethiciahas <lethiciahas@gmail.com>
2023-03-13 09:24:52 -0400

    adicionado referencias

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 export KUBECONFIG=$(for i in $(find /usr/bin -iname 'cluster1.yaml') ; do echo -n ":$i"; done | cut -c 2-)
 
+##ref: https://stackoverflow.com/questions/63629990/set-kubeconfig-environment-variable-dynamically##
+
 kubectl cluster-info
 
 spawn-fcgi -s /var/run/fcgiwrap.socket /usr/bin/fcgiwrap
