fb43f053b34c2b4cb637fe79cf9aa633d8451a14
Lethiciahas <lethiciahas@gmail.com>
2023-03-30 14:28:14 -0400

    alterações dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 export KUBECONFIG=$(for i in $(find /usr/bin -iname 'cluster1.yaml') ; do echo -n ":$i"; done | cut -c 2-)
 
 ##ref: https://stackoverflow.com/questions/63629990/set-kubeconfig-environment-variable-dynamically##
 
-kubectl cluster-info
+kubectl get nodes
 
 spawn-fcgi -s /var/run/fcgiwrap.socket /usr/bin/fcgiwrap
 chmod 777 /var/run/fcgiwrap.socket
--- dockerfile
+++ dockerfile
@@ -6,5 +6,5 @@ COPY fcgiwrap.conf /etc/nginx/
 COPY script-cgi /usr/lib/cgi-bin/
 RUN apk update && apk add spawn-fcgi fcgiwrap
 RUN chmod +x docker-entrypoint.sh
-
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 ENTRYPOINT ["sh", "/docker-entrypoint.sh"]
