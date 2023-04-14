cdc8f4999c51215e204e4a760712a5eb798f013b
Lethiciahas <lethiciahas@gmail.com>
2023-04-14 10:47:16 -0400

    add aquivos

--- kube-python/script.py
+++ kube-python/script.py
@@ -1,15 +1,16 @@
 from kubernetes import client, config
-from flask import Flask, Markup
+from flask import Flask
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+
 def get_namespaces():
     config.load_incluster_config()
     result = client.CoreV1Api()
     namespaces = [ns.metadata.name for ns in result.list_namespace().items]
-    namespaces_html = Markup('<ul>' + ''.join(f'<li>{ns}</li>' for ns in namespaces) + '</ul>')
+    namespaces_html = '<ul>' + ''.join(f'<li>{ns}</li>' for ns in namespaces) + '</ul>'
 
    
     return f"<meta http-equiv='refresh' content='10'>Listando Namespaces: {namespaces_html}"
